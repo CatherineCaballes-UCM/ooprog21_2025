ba0a0a19dc73b143ae4dbebd1ff8ddd6460d3552
CatherineCaballes-UCM <caballescatherine109@gmail.com>
2025-09-30 18:15:58 +0800

    Update DebugFour1.java

--- Chapter_4/DebugFour1.java
+++ Chapter_4/DebugFour1.java
@@ -1,46 +1,20 @@
 import java.util.Scanner;
 
-// Circle interface
-interface Circle {
-    double getArea();
-}
+public class DebugFour1 { 
 
-// DebugCircle class implementing Circle
-class DebugCircle implements Circle {
-    private double radius;
 
-    public DebugCircle(double r) {
-        radius = r;
-    }
-
-    public double getRad() {
-        return radius;
-    }
-
-    public double getDiam() {
-        return radius * 2;
-    }
-
-    @Override
-    public double getArea() {
-        return Math.PI * radius * radius;
-    }
-}
-
-// Main class with main method
-public class DebugFour1 {
-    public static void main(String[] args) {
+       public static void main(String[] args) 
+       {
         Scanner input = new Scanner(System.in);
-        double radius;
 
+        int radius;
+        
+        DebugCircle c = new DebugCircle(1);
         System.out.print("Enter a radius for a circle >> ");
-        radius = input.nextDouble();
-
-        DebugCircle c = new DebugCircle(radius);
-
-        System.out.println("The radius is " + c.getRad());
-        System.out.println("The diameter is " + c.getDiam());
+         radius = input.nextInt();
+         
+        System.out.println("The radius is " + c.getRadius());
+        System.out.println("The diameter is " + c.getDiameter());
         System.out.println("The area is " + c.getArea());
     }
 }
-
